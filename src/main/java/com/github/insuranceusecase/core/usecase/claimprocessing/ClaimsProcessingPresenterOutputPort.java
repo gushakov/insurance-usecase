fca107c3472623bd4d5373db4b34e091f6fdcf5d
gushakov <Georgy.Ushakov@unil.ch>
2025-05-28 15:23:54 +0200

    continue

--- src/main/java/com/github/insuranceusecase/core/usecase/claimprocessing/ClaimsProcessingPresenterOutputPort.java
+++ src/main/java/com/github/insuranceusecase/core/usecase/claimprocessing/ClaimsProcessingPresenterOutputPort.java
@@ -1,3 +1,18 @@
+/*
+    COPYRIGHT DISCLAIMER:
+    ----------
+    The code in this project has been inspired by the article
+    by Alistair Cockburn: "Structuring use cases with goals", 1995.
+    Article can be consulted from:
+    https://web.archive.org/web/20170620145208/http://alistair.cockburn.us/Structuring+use+cases+with+goals
+
+    The names of some methods and the terms used in the accompanying comments
+    are taken exactly as they figure in the original article by Alistair.
+    This is only for reasons of the illustration. All relevant copyrights
+    are presumed reserved by Alistair Cockburn.
+    Alistair's site: https://alistair.cockburn.us/.
+ */
+
 package com.github.insuranceusecase.core.usecase.claimprocessing;
 
 import com.github.insuranceusecase.core.model.claim.Claim;
--- src/main/java/com/github/insuranceusecase/core/usecase/claimprocessing/package-info.java
+++ src/main/java/com/github/insuranceusecase/core/usecase/claimprocessing/package-info.java
@@ -1,5 +1,22 @@
+/*
+    COPYRIGHT DISCLAIMER:
+    ----------
+    The code in this project has been inspired by the article
+    by Alistair Cockburn: "Structuring use cases with goals", 1995.
+    Article can be consulted from:
+    https://web.archive.org/web/20170620145208/http://alistair.cockburn.us/Structuring+use+cases+with+goals
+
+    The names of some methods and the terms used in the accompanying comments
+    are taken exactly as they figure in the original article by Alistair.
+    This is only for reasons of the illustration. All relevant copyrights
+    are presumed reserved by Alistair Cockburn.
+    Alistair's site: https://alistair.cockburn.us/.
+ */
+
 /**
- * "User goal" (Alistair Cockburn) for claim processing and related
- * objects will be placed in this folder.
+ * "User goal" (Alistair Cockburn) for claim filing and processing will be placed
+ * in this folder together with all accompanying classes:
+ * "Request Models"/"Response Models" (Robert C. Martin), input ports, presenter
+ * output ports, subcases, etc.
  */
 package com.github.insuranceusecase.core.usecase.claimprocessing;
